074e1f3b384a4ab6d683cbce941da78a7788a902
Thamsanqa Hadebe <thhadjhb025@student.wethinkcode.co.za>
2026-06-08 14:53:48 +0200

    update: added new entities in the entity package. Client, Portfolio, Security classes were created to represent those entities.

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,27 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @GeneratedValue()
+    private long clientId;
+
+    protected Portfolio() {}
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Long getClientId(){
+        return clientId;
+    }
+}
---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,92 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @GeneratedValue()
+    private long portflioId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private Date purchaseDate;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public Security(String name, String category, double purchasePrice, Date purchaseDate, int quantity) {
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    protected Security() {}
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public Long getPortfolioId() {
+        return portflioId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
